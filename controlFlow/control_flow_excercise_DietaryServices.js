dceafe5d5ded7166bc61bfc181e8b36e36de70e6
SWalAdv <127834869+SWalAdv@users.noreply.github.com>
2025-06-24 15:06:25 +0200

    Update control_flow_excercise_DietaryServices.js
    optimisation

--- controlFlow/control_flow_excercise_DietaryServices.js
+++ controlFlow/control_flow_excercise_DietaryServices.js
@@ -1,29 +1,33 @@
+// deigne rules and access types
 let userRole = "employee";
 let accessLevel;
 if (userRole === "employee") {
   accessLevel = "Access to Dietary Services";
 } else if (userRole === "Enrolled Member") {
-  accessLevel  "Access to Dietary Services & one-on-one interaction with a dietician";
+  accessLevel ="Access to Dietary Services & one-on-one interaction with a dietician";
 } else if (userRole === "Subscriber") {
   accessLevel = "Partial access to facilitate Dietary Services";
 } else {
   accessLevel = "No acces granted";
 }
 
+// Generate message for loggend-in users
 let isLogged = true
 let userMessage;
 if (isLogged) {
-   if (userRole === "employee");
-  {userMessage = "As an employee, you have access to Dietary Services."}
-  else if (userRole === "Enrolled Member"); 
-  {userMessage = "As an enrolled Member, you have access to Dietary Services & one-on-one interaction with a dietician."}
-  else if (userRole === "Subscriber");
-  {userMessage = "As a Subscriber, you have partial access to facilitate Dietary Services"};
-} else {userMessage = "Please log in to identify your Userstatus.";
+   if (userRole === "employee")
+    {userMessage = "As an employee, you have access to Dietary Services.";
+  } else if (userRole === "Enrolled Member") 
+    {userMessage = "As an enrolled Member, you have access to Dietary Services & one-on-one interaction with a dietician.";
+  } else if (userRole === "Subscriber")
+    {userMessage = "As a Subscriber, you have partial access to facilitate Dietary Services"};
+} else {userMessage = "Please log in to identify your Userstatus. If you are new, please enroll our subscribe forst to avail the facilities.";
 }
 
-let userType = "User";
+// Classify user by type using switch
+let userType = userRole;
 let userCategory;
+
 switch (userType) {
   case "employee":
     userCategory = "Employee";
@@ -34,5 +38,13 @@ switch (userType) {
   case "Subscriber":
     userCategory = "Subscriber"
     break;
-  default = "Unknown";
+  default :
+    userCategory = "Unknown";
+    break;
 }
+
+// Output everything
+console.log("User Role:", userRole);
+console.log("User Category:", userCategory);
+console.log("Access Level:", accessLevel);
+console.log("Message:", userMessage);
